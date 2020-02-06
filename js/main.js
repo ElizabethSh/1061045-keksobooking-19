58a55c69b1d2bc025b18b4261bb958ad0c1355b5
ElizabethSh <liza.shoshkina@gmail.com>
2020-02-06 18:26:39 +0100

    Исправлена валидация полей capacity и roomNumber

--- js/main.js
+++ js/main.js
@@ -85,28 +85,16 @@ var roomNumber = noticeForm.querySelector('#room_number');
 var capacity = noticeForm.querySelector('#capacity');
 
 var onRoomsCapasityChange = function () {
-  if (roomNumber.value === '1' && capacity.value === '1') {
-    roomNumber.setCustomValidity('');
-  } else {
+  if (roomNumber.value === '1' && capacity.value !== '1') {
     roomNumber.setCustomValidity('Для такого количества комнат можно выбрать 1 гостя');
-  }
-
-  if ((roomNumber.value === '2' && capacity.value === '1') || (roomNumber.value === '2' && capacity.value === '2')) {
-    roomNumber.setCustomValidity('');
-  } else {
+  } else if ((roomNumber.value === '2' && capacity.value < '1') || (roomNumber.value === '2' && capacity.value > '2')) {
     roomNumber.setCustomValidity('Для такого количества комнат можно выбрать 1 или 2 гостей');
-  }
-
-  if ((roomNumber.value === '3' && capacity.value === '1') || (roomNumber.value === '3' && capacity.value === '2') || (roomNumber.value === '3' && capacity.value === '3')) {
-    roomNumber.setCustomValidity('');
-  } else {
+  } else if (roomNumber.value === '3' && capacity.value < '1') {
     roomNumber.setCustomValidity('Для такого количества комнат можно выбрать 1, 2 или 3 гостей');
-  }
-
-  if (roomNumber.value === '100' && capacity.value === '0') {
-    roomNumber.setCustomValidity('');
-  } else {
+  } else if (roomNumber.value === '100' && capacity.value !== '0') {
     roomNumber.setCustomValidity('Для такого количества комнат можно выбрать вариант "не для гостей"');
+  } else {
+    roomNumber.setCustomValidity('');
   }
 };
 
