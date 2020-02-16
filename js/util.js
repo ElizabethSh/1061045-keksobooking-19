14869ee1a749cd59ac1bf13f74052e93c3a4e7d7
ElizabethSh <liza.shoshkina@gmail.com>
2020-02-16 08:58:48 +0100

    Добавляет функцию обработки нажатия ESC

--- js/util.js
+++ js/util.js
@@ -3,6 +3,7 @@
 (function () {
   var ENTER_KEYCODE = 13;
   var MOUSE_LEFT_KEYCODE = 1;
+  var ESC_KEYCODE = 27;
 
   var isEnterEvent = function (evt, action) {
     if (evt.keyCode === ENTER_KEYCODE) {
@@ -16,8 +17,15 @@
     }
   };
 
+  var isEscapeEvent = function (evt, action) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      action();
+    }
+  };
+
   window.util = {
     isEnterEvent: isEnterEvent,
-    isMouseLeftEvent: isMouseLeftEvent
+    isMouseLeftEvent: isMouseLeftEvent,
+    isEscapeEvent: isEscapeEvent
   };
 })();
