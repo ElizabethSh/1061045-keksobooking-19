cf43e939b6169569e52ab36122b27c3e6cb2d4bc
ElizabethSh <liza.shoshkina@gmail.com>
2020-02-16 09:04:38 +0100

    Добавляет функции обработки ошибок загрузки данных

--- js/page.js
+++ js/page.js
@@ -16,35 +16,44 @@
   var mapPinMainCoordinates = mapPinMain.style;
   var mapPinMainX = mapPinMainCoordinates.left.replace('px', '') * 1 + Math.round(MAIN_PIN_WIDTH / 2);
   var mapPinMainY = mapPinMainCoordinates.top.replace('px', '') * 1 + Math.round(MAIN_PIN_WIDTH / 2);
+  var main = document.querySelector('main'); // куда клонируем
+  var similarErrorTemplate = document.querySelector('#error') // нашли что клонировать
+                             .content
+                             .querySelector('div');
 
   // Переводит страницу в неактивное состояние
-  addressField.setAttribute('value', mapPinMainX + ', ' + mapPinMainY);
-  mapFilter.querySelector('fieldset').setAttribute('disabled', '');
+  var deactivatePage = function () {
+    addressField.setAttribute('value', mapPinMainX + ', ' + mapPinMainY);
+    mapFilter.querySelector('fieldset').setAttribute('disabled', '');
 
-  for (i = 0; i < mapFilterFields.length; i++) {
-    mapFilterFields[i].setAttribute('disabled', '');
-  }
+    for (i = 0; i < mapFilterFields.length; i++) {
+      mapFilterFields[i].setAttribute('disabled', '');
+    }
 
-  for (var i = 0; i < noticeFieldsets.length; i++) {
-    noticeFieldsets[i].setAttribute('disabled', '');
-  }
+    for (var i = 0; i < noticeFieldsets.length; i++) {
+      noticeFieldsets[i].setAttribute('disabled', '');
+    }
+  };
+
+  deactivatePage();
 
   // Переводит страницу в активное состояние
 
   var activatePage = function () {
+    window.backend.loading(successHandler, errorHandler);
     mapPinMainY = mapPinMainCoordinates.top.replace('px', '') * 1 + MAIN_PIN_HEIGHT;
 
     map.classList.remove('map--faded');
-    for (i = 0; i < mapFilterFields.length; i++) {
-      mapFilterFields[i].removeAttribute('disabled');
+    for (var i = 0; i < mapFilterFields.length; i++) {
+      mapFilterFields[i].removeAttribute('disabled'); // убирает из фильтра объявлений с selectов disabled
     }
-    mapFilter.querySelector('fieldset').removeAttribute('disabled');
-    noticeForm.classList.remove('ad-form--disabled');
+    mapFilter.querySelector('fieldset').removeAttribute('disabled'); // убирает из фильтра объявлений с fieldsetов disabled
+    noticeForm.classList.remove('ad-form--disabled'); // убирает disabled с формы
     for (i = 0; i < noticeFieldsets.length; i++) {
-      noticeFieldsets[i].removeAttribute('disabled');
+      noticeFieldsets[i].removeAttribute('disabled'); // убирает из объявлений с selectов disabled
     }
-    window.createPin();
-    addressField.setAttribute('value', mapPinMainX + ', ' + mapPinMainY);
+    // window.createPin(); // создает пины на карте
+    addressField.setAttribute('value', mapPinMainX + ', ' + mapPinMainY); // при активации страницы меняет координату Y main-pin
   };
 
   mapPinMain.addEventListener('mousedown', function () {
@@ -54,4 +63,34 @@
   mapPinMain.addEventListener('keydown', function (evt) {
     window.util.isEnterEvent(evt, activatePage);
   });
+
+  var closeErrorMessage = function () {
+    var errorWindow = document.querySelector('.error');
+    errorWindow.remove();
+  };
+
+  var successHandler = function (announcements) {
+    window.createPin(announcements);
+  };
+
+  var errorHandler = function () {
+    var similarErrorWindow = similarErrorTemplate.cloneNode(true); // клонируем шаблон
+    main.appendChild(similarErrorWindow); // рисуем сообщение
+    var errorButton = document.querySelector('.error__button');
+    window.page.deactivatePage();
+    errorButton.addEventListener('click', function () { // закрывает окно с ошибкой по клику на кнопку
+      closeErrorMessage();
+    });
+    errorButton.addEventListener('keydown', function (evt) { // закрывает окно с ошибкой по Enter на кнопку
+      window.util.isEnterEvent(evt, closeErrorMessage);
+    });
+    document.addEventListener('keydown', function (evt) { // закрывает окно с ошибкой по ESC
+      window.util.isEscapeEvent(evt, closeErrorMessage);
+    });
+  };
+
+  window.page = {
+    deactivatePage: deactivatePage,
+    activatePage: activatePage
+  };
 })();
