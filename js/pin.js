0081d9cc316ec5635b116387b5042e74e331a5a8
ElizabethSh <liza.shoshkina@gmail.com>
2020-02-20 13:48:02 +0100

    Добавляет код для перемещения пина по карте

--- js/pin.js
+++ js/pin.js
@@ -30,4 +30,43 @@
     // similarListElement.appendChild(fragment);
   };
 
+  // перемещение главного пина на карте
+
+  var mainPinHandler = document.querySelector('.map__pin--main');
+  mainPinHandler.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      mainPinHandler.style.top = (mainPinHandler.offsetTop - shift.y) + 'px';
+      mainPinHandler.style.left = (mainPinHandler.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
 })();
