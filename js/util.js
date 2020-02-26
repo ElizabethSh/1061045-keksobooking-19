1e2c22845c7db51dd7793a93bb4873ceaea7665d
ElizabethSh <liza.shoshkina@gmail.com>
2020-02-26 08:40:50 +0100

    Добавляет переменные с размерами пина

--- js/util.js
+++ js/util.js
@@ -5,6 +5,10 @@
   var MOUSE_LEFT_KEYCODE = 1;
   var ESC_KEYCODE = 27;
 
+  var MAIN_PIN_WIDTH = document.querySelector('.map__pin--main').offsetWidth;
+  var MAIN_PIN_HEIGHT = 81; // document.querySelector('.map__pin--main').scrollHeight;
+  var MAIN_PIN_RADIUS = Math.round(MAIN_PIN_WIDTH / 2);
+
   var isEnterEvent = function (evt, action) {
     if (evt.keyCode === ENTER_KEYCODE) {
       action();
@@ -32,6 +36,10 @@
   };
 
   window.util = {
+    MAIN_PIN_WIDTH: MAIN_PIN_WIDTH,
+    MAIN_PIN_HEIGHT: MAIN_PIN_HEIGHT,
+    MAIN_PIN_RADIUS: MAIN_PIN_RADIUS,
+
     isEnterEvent: isEnterEvent,
     isMouseLeftEvent: isMouseLeftEvent,
     isEscapeEvent: isEscapeEvent,
