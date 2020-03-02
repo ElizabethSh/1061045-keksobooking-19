4931bc11538cc4b8b47f8fc79fea3ab8f13c39ee
ElizabethSh <liza.shoshkina@gmail.com>
2020-03-02 18:49:55 +0100

    Перенесла из другого модуля функции создания и удаления пинов

--- js/pin.js
+++ js/pin.js
@@ -3,10 +3,13 @@
 (function () {
   var PIN_WIDTH = 50;
   var PIN_HEIGHT = 70;
+  var PIN_AMOUNT_MAX = 5;
 
   var similarAnnouncementTemplate = document.querySelector('#pin')
         .content
         .querySelector('button');
+  var map = document.querySelector('.map');
+  var mapPinsList = map.querySelector('.map__pins');
 
   var renderPin = function (announcement, index) {
     var announcementElement = similarAnnouncementTemplate.cloneNode(true);
@@ -21,7 +24,25 @@
     return announcementElement;
   };
 
+  var removePins = function () {
+    var pins = mapPinsList.querySelectorAll('button[type="button"]');
+    pins.forEach(function (it) {
+      it.remove();
+    });
+  };
+
+  // Функция создания пинов на карте
+  var createPin = function (data) {
+    var takeNumber = data.length > PIN_AMOUNT_MAX ? PIN_AMOUNT_MAX : data.length; // выводить не больше 5 элементов
+    // var fragment = document.createDocumentFragment();
+    removePins();
+    for (var k = 0; k < takeNumber; k++) {
+      mapPinsList.appendChild(renderPin(data[k], k));
+    }
+  };
+
   window.pin = {
-    renderPin: renderPin
+    create: createPin,
+    remove: removePins
   };
 })();
