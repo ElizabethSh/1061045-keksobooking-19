bc00da927e27c95a694587a60018c36d9bc44f85
ElizabethSh <liza.shoshkina@gmail.com>
2020-03-05 13:24:27 +0100

    Добавлено перечисление и экспортированы глобальные переменные для данных с сервера

--- js/util.js
+++ js/util.js
@@ -1,28 +1,34 @@
 'use strict';
 
 (function () {
-  var ENTER_KEYCODE = 13;
-  var MOUSE_LEFT_KEYCODE = 1;
-  var ESC_KEYCODE = 27;
 
   var MAIN_PIN_WIDTH = document.querySelector('.map__pin--main').offsetWidth;
-  var MAIN_PIN_HEIGHT = 81; // document.querySelector('.map__pin--main').scrollHeight;
-  var MAIN_PIN_RADIUS = Math.round(MAIN_PIN_WIDTH / 2);
+  var MAIN_PIN_HEIGHT = 81;
+  var MAIN_PIN_RADIUS = Math.round(MAIN_PIN_WIDTH / 2); // 33
+
+  var Keycode = {
+    ENTER: 13,
+    ESC: 27,
+    MOUSE_LEFT: 1
+  };
+
+  var announcements = [];
+  var filteredData = [];
 
   var isEnterEvent = function (evt, action) {
-    if (evt.keyCode === ENTER_KEYCODE) {
+    if (evt.keyCode === Keycode.ENTER) {
       action();
     }
   };
 
   var isMouseLeftEvent = function (action) {
-    if (event.which === MOUSE_LEFT_KEYCODE) {
+    if (event.which === Keycode.MOUSE_LEFT) {
       action();
     }
   };
 
   var isEscapeEvent = function (evt, action) {
-    if (evt.keyCode === ESC_KEYCODE) {
+    if (evt.keyCode === Keycode.ESC) {
       action();
     }
   };
@@ -31,19 +37,17 @@
     document.querySelector(className).remove();
   };
 
-  var closePopup = function (className) {
-    document.querySelector(className).classList.add('hidden');
-  };
-
   window.util = {
     MAIN_PIN_WIDTH: MAIN_PIN_WIDTH,
     MAIN_PIN_HEIGHT: MAIN_PIN_HEIGHT,
     MAIN_PIN_RADIUS: MAIN_PIN_RADIUS,
 
+    announcements: announcements,
+    filteredData: filteredData,
+
     isEnterEvent: isEnterEvent,
     isMouseLeftEvent: isMouseLeftEvent,
     isEscapeEvent: isEscapeEvent,
     removeElement: removeElement,
-    closePopup: closePopup
   };
 })();
