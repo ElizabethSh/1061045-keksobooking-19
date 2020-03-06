a744cb8afdb5574af53a113a82e152bde4db8279
ElizabethSh <liza.shoshkina@gmail.com>
2020-03-06 17:04:21 +0100

    Исправляет координаты пинов объявлений

--- js/pin.js
+++ js/pin.js
@@ -14,8 +14,8 @@
   var renderPin = function (index) {
     var announcementElement = similarAnnouncementTemplate.cloneNode(true);
     var announcement = window.util.announcements[index];
-    announcementElement.style.left = announcement.location.x + PIN_WIDTH / 2 + 'px';
-    announcementElement.style.top = announcement.location.y + PIN_HEIGHT + 'px';
+    announcementElement.style.left = announcement.location.x - PIN_WIDTH / 2 + 'px';
+    announcementElement.style.top = announcement.location.y - PIN_HEIGHT + 'px';
     announcementElement.querySelector('img').setAttribute('src', announcement.author.avatar);
     announcementElement.querySelector('img').setAttribute('alt', announcement.offer.title);
     announcementElement.querySelector('img').setAttribute('key', index);
