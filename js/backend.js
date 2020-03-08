0135a546a61d5c0828a2bfbf7aacdbd75fe9b7cd
ElizabethSh <liza.shoshkina@gmail.com>
2020-03-08 15:34:44 +0100

    Добавляет функцию настройки xhr запроса

--- js/backend.js
+++ js/backend.js
@@ -2,12 +2,17 @@
 
 (function () {
   var SUCCESS_CODE = 200;
+  var TIMEOUT = 10000;
+  var URL_GET = 'https://js.dump.academy/keksobooking/data';
+  var URL_POST = 'https://js.dump.academy/keksobooking';
 
-  var load = function (URL, onSuccess, onError) {
+
+  // функция настройки xhr запроса
+
+  var setup = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
 
     xhr.responseType = 'json';
-
     xhr.addEventListener('load', function () {
       if (xhr.status === SUCCESS_CODE) {
         onSuccess(xhr.response);
@@ -23,34 +28,24 @@
       onError();
     });
 
-    xhr.timeout = 10000;
+    xhr.timeout = TIMEOUT;
 
-    xhr.open('GET', URL);
+    return xhr;
+  };
+
+  // функция загрузки данных с сервера
+
+  var load = function (onSuccess, onError) {
+    var xhr = setup(onSuccess, onError);
+
+    xhr.open('GET', URL_GET);
     xhr.send();
   };
 
-  var URL_POST = 'https://js.dump.academy/keksobooking';
+  // функция отправки данных из формы
 
   var send = function (data, onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === SUCCESS_CODE) {
-        onSuccess(xhr.response);
-      } else {
-        onError();
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      onError();
-    });
-    xhr.addEventListener('timeout', function () {
-      onError();
-    });
-
-    xhr.timeout = 10000;
+    var xhr = setup(onSuccess, onError);
 
     xhr.open('POST', URL_POST);
     xhr.send(data);
