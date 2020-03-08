5afb8d2d87d04c173d0e4741d6097ffe65c4cde3
ElizabethSh <liza.shoshkina@gmail.com>
2020-03-08 15:42:22 +0100

    Удаляет фильтрацию данных (перенесено в модуль data.js)

--- js/page.js
+++ js/page.js
@@ -1,7 +1,6 @@
 'use strict';
 
 (function () {
-  var URL_GET = 'https://js.dump.academy/keksobooking/data';
 
   var map = document.querySelector('.map');
   var mapPinMain = document.querySelector('.map__pin--main');
@@ -10,17 +9,16 @@
   var mapFilterFields = mapFilter.querySelectorAll('select');
   var notice = document.querySelector('.notice');
   var noticeFieldsets = notice.querySelectorAll('fieldset');
-  var addressField = noticeForm.querySelector('#address'); // находим поле адреса
 
   // определяем координаты левого верхнего угла пина
   var mapPinMainCoordinates = {
-    x: mapPinMain.style.left.replace('px', ''),
-    y: mapPinMain.style.top.replace('px', '')
+    x: mapPinMain.offsetLeft,
+    y: mapPinMain.offsetTop
   };
 
   // определяем координаты центра пина
-  var mapPinMainX = mapPinMainCoordinates.x * 1 + window.util.MAIN_PIN_RADIUS;
-  var mapPinMainY = mapPinMainCoordinates.y * 1 + window.util.MAIN_PIN_RADIUS;
+  var mapPinMainX = mapPinMainCoordinates.x + window.util.MAIN_PIN_RADIUS;
+  var mapPinMainY = mapPinMainCoordinates.y + window.util.MAIN_PIN_RADIUS;
   //
   var main = document.querySelector('main'); // куда клонируем
   var similarErrorTemplate = document.querySelector('#error') // нашли что клонировать
@@ -29,22 +27,19 @@
 
 
   // Функция перевода страницы в неактивное состояние
-
   var deactivate = function () {
     map.classList.add('map--faded');
     noticeForm.classList.add('ad-form--disabled');
 
     // запоминаем начальные координаты пина до его перемещения
-
     mapPinMain.style.top = mapPinMainCoordinates.y + 'px';
     mapPinMain.style.left = mapPinMainCoordinates.x + 'px';
 
     // при деактивайии координата Y меняется (центр метки)
-    mapPinMainY = mapPinMainCoordinates.y * 1 + window.util.MAIN_PIN_RADIUS;
+    mapPinMainY = mapPinMainCoordinates.y + window.util.MAIN_PIN_RADIUS;
 
     // подставляем координаты в поле адрес
-    addressField.setAttribute('value', mapPinMainX + ', ' + mapPinMainY);
-
+    window.form.fillAddressField(mapPinMainX, mapPinMainY);
     mapFilter.querySelector('fieldset').setAttribute('disabled', '');
 
     mapFilterFields.forEach(function (it) {
@@ -59,13 +54,13 @@
 
   deactivate();
 
-  // Переводит страницу в активное состояние
 
+  // Переводит страницу в активное состояние
   var activate = function () {
-    window.backend.load(URL_GET, successHandler, errorHandler);
+    window.backend.load(successHandler, errorHandler);
 
     // при активации координата Y изменяется (указатель пина)
-    mapPinMainY = mapPinMainCoordinates.y * 1 + window.util.MAIN_PIN_HEIGHT;
+    mapPinMainY = mapPinMainCoordinates.y + window.util.MAIN_PIN_HEIGHT;
 
     map.classList.remove('map--faded');
 
@@ -79,8 +74,7 @@
       it.removeAttribute('disabled'); // убирает из объявлений с selectов disabled
     });
 
-    addressField.setAttribute('value', mapPinMainX + ', ' + mapPinMainY); // при активации страницы меняет координату в поле адрес
-
+    window.form.fillAddressField(mapPinMainX, mapPinMainY);
     mapPinMain.removeEventListener('mousedown', onPinMousedownPress);
   };
 
@@ -96,98 +90,22 @@
     window.util.isEnterEvent(evt, activate);
   });
 
-  var housingType = mapFilter.querySelector('#housing-type');
-  var housingPrice = mapFilter.querySelector('#housing-price');
-  var housingRoom = mapFilter.querySelector('#housing-rooms');
-  var housingGuests = mapFilter.querySelector('#housing-guests');
-
-  var propertyChoice;
-  var priceChoice;
-  var roomChoice;
-  var guestChoice;
-
-  mapFilter.addEventListener('change', function () {
-
-    window.card.remove();
-    propertyChoice = housingType.value;
-    priceChoice = housingPrice.value;
-    roomChoice = parseInt(housingRoom.value, 10);
-    guestChoice = parseInt(housingGuests.value, 10);
-
-    window.debounce(updateData);
-  });
-
-
-  var updateData = function () {
-    var filteredData = [];
-
-    // получаем массив выбранных удобств
-    var pickedFeatures = [];
-    var inputFeaturesChecked = mapFilter.querySelectorAll('input[type="checkbox"]:checked');
-
-    inputFeaturesChecked.forEach(function (it) {
-      pickedFeatures.push(it.value);
-    });
-
-    // определение соответствия категории цены
-    for (var i = 0; i < window.util.announcements.length; i++) {
-      var it = window.util.announcements[i];
-      if (it.offer.price >= 10000 && it.offer.price <= 50000) {
-        var chosenPrise = 'middle';
-      } else if (it.offer.price < 10000) {
-        chosenPrise = 'low';
-      } else if (it.offer.price > 50000) {
-        chosenPrise = 'high';
-      } else {
-        chosenPrise = 'any';
-      }
-
-      // функция определения, есть ли в объявленях выбранное удобство
-
-      var isFeature = function () {
-        var pikedFeature = true;
-        for (var j = 0; j < pickedFeatures.length; j++) {
-          var isAddToFilteredData = it.offer.features.includes(pickedFeatures[j]);
-          if (!isAddToFilteredData) {
-            pikedFeature = false;
-            break;
-          }
-        }
-        return pikedFeature;
-      };
-
-      if ((it.offer.type === propertyChoice || housingType.value === 'any') &&
-      (it.offer.rooms === roomChoice || housingRoom.value === 'any') &&
-      (it.offer.guests === guestChoice || housingGuests.value === 'any') &&
-      (chosenPrise === priceChoice || housingPrice.value === 'any') && (isFeature())) {
-        filteredData.push(i);
-      }
-    }
-
-    window.pin.create(filteredData);
-  };
-
   // функция успешной загрузки данных
   var successHandler = function (data) {
-    window.util.announcements = data; // сохраняем загруженные данные в перем. announcements
-    for (var k = 0; k < window.util.announcements.length; k++) {
-      var ann = window.util.announcements[k];
-      if (Object.keys(ann.offer).length !== 0) {
-        window.util.filteredData.push(k);
-      }
-    }
-    window.pin.create(window.util.filteredData);
+    window.data.announcements = data; // сохраняем загруженные данные в перем. announcements
+
+    // если в объявлении не заполнен раздел offer, пин не отрисовывается
+    window.data.isOfferFilled();
+    window.pin.create(window.data.filteredData);
   };
 
   // функция закрытия окна ошибки
-
   var closeErrorMessage = function () {
     var errorWindow = document.querySelector('.error');
     errorWindow.remove();
   };
 
   // функция ошибки загрузки данных
-
   var errorHandler = function () {
     var similarErrorWindow = similarErrorTemplate.cloneNode(true); // клонируем шаблон
     main.appendChild(similarErrorWindow); // рисуем сообщение
