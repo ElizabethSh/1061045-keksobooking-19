3a19eae323370a35baa7857d7e653903a72c313a
ElizabethSh <liza.shoshkina@gmail.com>
2020-03-10 17:35:31 +0100

    Коллбэк перемещения пина вынесен в отдельную функцию

--- js/map.js
+++ js/map.js
@@ -1,20 +1,16 @@
 'use strict';
 
 (function () {
-
   var X_MIN = 0;
   var X_MAX = 1200;
   var Y_MIN = 130;
   var Y_MAX = 630;
 
-  var map = document.querySelector('.map');
-  var mapPinsList = map.querySelector('.map__pins');
-
   var onPinPress = function (evt) {
     // По нажатию на пин объявлений создаем соответствующую ему карточку
     if (evt.target && (evt.target.matches('img[key]') || evt.target.matches('button[type="button"]'))) {
       window.card.create(evt.target.getAttribute('key'));
-      var mapPinActive = mapPinsList.querySelector('.map__pin--active');
+      var mapPinActive = window.pin.mapPinsList.querySelector('.map__pin--active');
       if (mapPinActive) {
         mapPinActive.classList.remove('map__pin--active');
       }
@@ -27,13 +23,11 @@
     }
   };
 
-
-  mapPinsList.addEventListener('click', onPinPress);
+  window.pin.mapPinsList.addEventListener('click', onPinPress);
 
   // перемещение главного пина на карте
 
-  var mainPinHandler = document.querySelector('.map__pin--main');
-  mainPinHandler.addEventListener('mousedown', function (evt) {
+  var onMouseDown = function (evt) {
     evt.preventDefault();
 
     var startCoords = {
@@ -60,24 +54,24 @@
       };
 
       var coordsForAddressField = {
-        x: mainPinHandler.offsetLeft + window.util.MAIN_PIN_RADIUS,
-        y: mainPinHandler.offsetTop + window.util.MAIN_PIN_HEIGHT
+        x: mainPinHandler.offsetLeft + window.pin.mainPinRadius,
+        y: mainPinHandler.offsetTop + window.pin.MAIN_PIN_HEIGHT
       };
 
       window.form.fillAddressField(coordsForAddressField.x, coordsForAddressField.y);
 
-      if (currentCoords.x < X_MIN - window.util.MAIN_PIN_RADIUS) {
-        mainPinHandler.style.left = X_MIN - window.util.MAIN_PIN_RADIUS + 'px';
-      } else if (currentCoords.x > X_MAX - window.util.MAIN_PIN_RADIUS) {
-        mainPinHandler.style.left = (X_MAX - window.util.MAIN_PIN_RADIUS) + 'px';
+      if (currentCoords.x < X_MIN - window.pin.mainPinRadius) {
+        mainPinHandler.style.left = X_MIN - window.pin.mainPinRadius + 'px';
+      } else if (currentCoords.x > X_MAX - window.pin.mainPinRadius) {
+        mainPinHandler.style.left = (X_MAX - window.pin.mainPinRadius) + 'px';
       } else {
         mainPinHandler.style.left = currentCoords.x + 'px';
       }
 
-      if (currentCoords.y < Y_MIN - window.util.MAIN_PIN_HEIGHT) {
-        mainPinHandler.style.top = Y_MIN - window.util.MAIN_PIN_HEIGHT + 'px';
-      } else if (currentCoords.y > Y_MAX - window.util.MAIN_PIN_HEIGHT) {
-        mainPinHandler.style.top = Y_MAX - window.util.MAIN_PIN_HEIGHT + 'px';
+      if (currentCoords.y < Y_MIN - window.pin.MAIN_PIN_HEIGHT) {
+        mainPinHandler.style.top = Y_MIN - window.pin.MAIN_PIN_HEIGHT + 'px';
+      } else if (currentCoords.y > Y_MAX - window.pin.MAIN_PIN_HEIGHT) {
+        mainPinHandler.style.top = Y_MAX - window.pin.MAIN_PIN_HEIGHT + 'px';
       } else {
         mainPinHandler.style.top = currentCoords.y + 'px';
       }
@@ -92,5 +86,21 @@
 
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
-  });
+  };
+
+  var mainPinHandler = document.querySelector('.map__pin--main');
+  mainPinHandler.addEventListener('mousedown', onMouseDown);
+
+  var removeListner = function () {
+    mainPinHandler.removeEventListener('mousedown', onMouseDown);
+  };
+
+  var addListner = function () {
+    mainPinHandler.addEventListener('mousedown', onMouseDown);
+  };
+
+  window.map = {
+    removeListner: removeListner,
+    addListner: addListner
+  };
 })();
